cf7faaba7534955bd479e90faa3a890ab85e54a8
Aleksandr Grigorev <cognize6@gmail.com>
2021-08-05 13:47:14 +0300

    Change the origin to bitzlato.com

--- web/public/config/env.staging.js
+++ web/public/config/env.staging.js
@@ -1,22 +1,22 @@
 window.env = {
     api: {
-      authUrl: 'https://ex-stage.bitzlato.bz/api/v2/barong',
-      tradeUrl: 'https://ex-stage.bitzlato.bz/api/v2/peatio',
-      finexUrl: 'https://ex-stage.bitzlato.bz/api/v2/finex',
-      applogicUrl: 'https://ex-stage.bitzlato.bz/api/v2/applogic',
-      rangerUrl: 'wss://ex-stage.bitzlato.bz/api/v2/ranger',
+      authUrl: 'https://ex-stage.bitzlato.com/api/v2/barong',
+      tradeUrl: 'https://ex-stage.bitzlato.com/api/v2/peatio',
+      finexUrl: 'https://ex-stage.bitzlato.com/api/v2/finex',
+      applogicUrl: 'https://ex-stage.bitzlato.com/api/v2/applogic',
+      rangerUrl: 'wss://ex-stage.bitzlato.com/api/v2/ranger',
     },
     auth0: {
       domain: 'bitzlato-dev.auth0.com',
       client_id: 'OL926gD0Zha6h80uJx4TVhJLMKrJemjb',
-      redirect_uri: 'https://ex-stage.bitzlato.bz/success_signin',
-      auth_url: 'https://ex-stage.bitzlato.bz/api/v2/barong/identity/sessions/auth0',
-      signedin_url: 'https://ex-stage.bitzlato.bz/wallets'
+      redirect_uri: 'https://ex-stage.bitzlato.com/success_signin',
+      auth_url: 'https://ex-stage.bitzlato.com/api/v2/barong/identity/sessions/auth0',
+      signedin_url: 'https://ex-stage.bitzlato.com/wallets'
     },
     signInUrl: '/signin/bitzlato.html',
     signUpUrl: '/signin/bitzlato.html',
-    logoUrl: 'https://market.bitzlato.bz/assets/bitzlato_logo--sm--blue--nav.svg',
-    logoDarkUrl: 'https://market.bitzlato.bz/assets/bitzlato_logo--sm--white--nav.svg',
+    logoUrl: 'https://market.bitzlato.com/assets/bitzlato_logo--sm--blue--nav.svg',
+    logoDarkUrl: 'https://market.bitzlato.com/assets/bitzlato_logo--sm--white--nav.svg',
     minutesUntilAutoLogout: '120',
     withCredentials: false,
     finex: false,
@@ -32,7 +32,7 @@ window.env = {
     balancesFetchInterval: '3000',
     passwordEntropyStep: 14,
     showLanding: true,
-    directSigninUrl: 'https://bitzlato.bz/auth/login?ex=true&returnTo=https%3A%2F%2Fmarket.bitzlato.bz%2Fsuccess_signin%2Fbitzlato.html',
+    directSigninUrl: 'https://bitzlato.com/auth/login?ex=true&returnTo=https%3A%2F%2Fmarket.bitzlato.com%2Fsuccess_signin%2Fbitzlato.html',
     sentryEnabled: false,
     kycSteps: [
       'email',
