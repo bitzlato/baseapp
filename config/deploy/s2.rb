5276d23d34c3868a302499f5f23ef944657cfb95
mailer21 <metelew@gmail.com>
2021-08-20 12:06:15 +0300

    fix deploy to s2

--- config/deploy/s2.rb
+++ config/deploy/s2.rb
@@ -3,5 +3,6 @@
 set :stage, :staging
 set :public_url, 'https://market-s2.bitzlato.com/'
 set :build_domain, 'market-s2.bitzlato.com'
+set :stage, 's2'
 
 server ENV.fetch( 'STAGING_SERVER_2' ), user: fetch(:user), roles: fetch(:roles)
