bf20610a5ea92b9eb28b522d0bdc4ae10e5ca011
Danil Pismenny <danil@brandymint.ru>
2021-08-31 14:26:15 +0300

    Migrate from ENV[ to ENV.fetch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :user, 'baseapp'
 set :public_url, 'https://market.bitzlato.bz/'
 set :build_domain, 'market.bitzlato.bz'
 
-server ENV['PRODUCTION_SERVER'],
+server ENV.fetch( 'PRODUCTION_SERVER' ),
        user: fetch(:user),
        roles: fetch(:roles),
        ssh_options: { forward_agent: true }
