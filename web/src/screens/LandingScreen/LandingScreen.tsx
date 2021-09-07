cbda2e346c9db5de4b8118113f1450eae1f6c46e
Aleksandr Grigorev <cognize6@gmail.com>
2021-09-07 10:50:22 +0300

    Remove 'Powered by' block from the landing screen

--- web/src/screens/LandingScreen/LandingScreen.tsx
+++ web/src/screens/LandingScreen/LandingScreen.tsx
@@ -5,18 +5,14 @@ import { WelcomeBlock } from './WelcomeBlock';
 import { ProjectInfoBlock } from './ProjectInfoBlock';
 import { JoinBlock } from './JoinBlock';
 import { FeaturesBlock } from './FeaturesBlock';
-import { PoweredByOpenwareBlock } from './PoweredByOpenwareBlock';
 
 export const LandingScreen: FC = () => {
-    // TODO: add header and footer
-
     return (
         <div className={s.landing}>
             <WelcomeBlock />
             <ProjectInfoBlock />
             <JoinBlock />
             <FeaturesBlock />
-            <PoweredByOpenwareBlock />
         </div>
     );
 };
