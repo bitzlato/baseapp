66e0c3e13309f34fdbb88b9b2d2c740aa0d5b148
Maxim Gerasimov <treizeger@gmail.com>
2021-09-07 21:33:47 +0300

    Add beta mark

--- web/src/components/Logo/index.tsx
+++ web/src/components/Logo/index.tsx
@@ -1,9 +1,12 @@
 import React, { FC, ReactElement, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import cn from 'classnames';
+import { Beta } from 'src/assets/images/Beta';
 import { LogoIcon } from '../../assets/images/LogoIcon';
 import { selectApplyWindowEnvsTriggerState } from '../../modules';
 import { CustomizationSettingsInterface, LogoInterface } from '../../themes';
 
+import s from './Logo.postcss';
 
 const DEFAULT_IMAGE: LogoInterface = {
     url: '',
@@ -15,8 +18,9 @@ export const Logo: FC = (): ReactElement => {
     const [image, setImage] = useState<LogoInterface | undefined>(DEFAULT_IMAGE);
 
     const handleGetImageFromConfig = (): LogoInterface | undefined => {
-        const settingsFromConfig: CustomizationSettingsInterface | undefined =
-            window.env?.palette ? JSON.parse(window.env.palette) : undefined;
+        const settingsFromConfig: CustomizationSettingsInterface | undefined = window.env?.palette
+            ? JSON.parse(window.env.palette)
+            : undefined;
 
         return settingsFromConfig?.['header_logo'];
     };
@@ -26,20 +30,20 @@ export const Logo: FC = (): ReactElement => {
     }, [applyWindowEnvsTrigger]);
 
     return (
-        <div className="pg-logo">
+        <div className={cn('pg-logo', s.logo)}>
             {image?.url ? (
                 <img
                     src={image.url}
                     alt="Logo"
                     className="pg-logo__img"
-                    style={{ width: image?.width ? `${image.width}px` : 'auto'}}
+                    style={{ width: image?.width ? `${image.width}px` : 'auto' }}
                 />
             ) : (
-                <LogoIcon
-                    className="pg-logo__img"
-                    styles={{ width: image?.width ? `${image.width}px` : 'auto'}}
-                />
+                <LogoIcon className="pg-logo__img" styles={{ width: image?.width ? `${image.width}px` : 'auto' }} />
             )}
+            <div className={s.beta}>
+                <Beta />
+            </div>
         </div>
     );
 };
