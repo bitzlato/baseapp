fbe46fe02a5ddbd29a7d03a3e375a9d7419aff31
Maxim Gerasimov <treizeger@gmail.com>
2021-09-10 18:21:22 +0300

    Move sign in / sign up links

--- web/src/containers/NavBar/NavBar.tsx
+++ web/src/containers/NavBar/NavBar.tsx
@@ -1,20 +1,39 @@
 import React, { FC } from 'react';
-
 import { useSelector } from 'react-redux';
-
 import { selectUserLoggedIn } from 'src/modules';
+import { useT } from 'src/hooks/useT';
+import { Link } from 'react-router-dom';
+import { Button } from 'src/components/Button/Button';
 
 import { LanguageSwitcher } from './LanguageSwitcher/LanguageSwitcher';
 import { ThemeSwitcher } from './ThemeSwitcher/ThemeSwitcher';
 import { ToProfileButton } from './ToProfileButton/ToProfileButton';
-import s from './NavBar.postcss';
 import { LogoutButton } from './LogoutButton/LogoutButton';
 
+import s from './NavBar.postcss';
+
 export const NavBar: FC = () => {
+    const t = useT();
     const isLoggedIn = useSelector(selectUserLoggedIn);
 
     return (
         <div className={s.navBar}>
+            {!isLoggedIn && (
+                <>
+                    <div className={s.item}>
+                        <Button component={Link} to="/signin">
+                            {t('page.header.navbar.signIn')}
+                        </Button>
+                    </div>
+                    <div className={s.item}>
+                        {/* signup ID is needed for GA event */}
+                        <Button id="signup" color="secondary" component={Link} to="/signup">
+                            {t('page.header.signUp')}
+                        </Button>
+                    </div>
+                </>
+            )}
+
             <div className={s.item}>
                 <ThemeSwitcher />
             </div>
