3ee8ea9a4432ff3c503cf058b2f347df97ff95e2
Aleksandr Grigorev <cognize6@gmail.com>
2021-09-14 14:36:56 +0300

    Add Swagger tab to the documentation screen

--- web/src/screens/DocumentationScreen/index.tsx
+++ web/src/screens/DocumentationScreen/index.tsx
@@ -1,21 +1,44 @@
 import * as React from 'react';
-import {
-    DocumentationEndpoints,
-    DocumentationHeader,
-    DocumentationModels,
-} from '../../components';
+import { MarketSelectorTab } from 'src/containers/MarketSelector/MarketSelectorTabs/MarketSelectorTab';
+import { DocumentationEndpoints, DocumentationHeader, DocumentationModels } from '../../components';
 import { useDocumentationFetch } from '../../hooks';
 
 export const DocumentationScreen: React.FC = () => {
-    useDocumentationFetch();
+    const [activeTab, setActiveTab] = React.useState('0');
 
     return (
         <div className="pg-documentation">
             <div className="pg-documentation__content">
-                <DocumentationHeader />
-                <DocumentationEndpoints />
-                <DocumentationModels />
+                <div className='cr-row'>
+                    <MarketSelectorTab id="0" onClick={setActiveTab} activeId={activeTab}>
+                        {'API'}
+                    </MarketSelectorTab>
+                    <MarketSelectorTab id="1" onClick={setActiveTab} activeId={activeTab}>
+                        {'Swagger UI'}
+                    </MarketSelectorTab>
+                </div>
+                {activeTab === '0' && <Doc />}
+                {activeTab === '1' && (
+                    <React.Suspense fallback>
+                        <Swagger />
+                    </React.Suspense>
+                )}
             </div>
         </div>
     );
 };
+
+const Doc: React.FC = () => {
+    useDocumentationFetch();
+    return (
+        <>
+            <DocumentationHeader />
+            <DocumentationEndpoints />
+            <DocumentationModels />
+        </>
+    );
+};
+
+const Swagger = React.lazy(() =>
+    import(/* webpackChunkName: "swagger" */ './Swagger').then(m => ({ default: m.Swagger }))
+);
