eaaad7fa744d5acc87956f29fe7b88a4fc08158b
Maxim Gerasimov <treizeger@gmail.com>
2021-09-21 23:11:00 +0300

    Fix setUser for bugsnag

--- web/src/containers/ErrorBoundary/ErrorBoundary.tsx
+++ web/src/containers/ErrorBoundary/ErrorBoundary.tsx
@@ -2,10 +2,11 @@ import React, { ReactNode, useEffect, ComponentType, ReactElement } from 'react'
 import Bugsnag from '@bugsnag/js';
 import BugsnagPluginReact from '@bugsnag/plugin-react';
 import { useSelector } from 'react-redux';
-import { selectUserInfo } from 'src/modules';
+import { selectUserInfo, RootState, selectUserLoggedIn } from 'src/modules';
 import { ErrorWrapper } from 'src/containers/Errors';
 
 let BugsnagErrorBoundary: ComponentType | undefined;
+let bugsnagUser: { uid: string; email: string; username: string } | undefined;
 const bugsnagKey = process.env.REACT_APP_BUGSNAG_KEY;
 if (typeof bugsnagKey === 'string' && bugsnagKey !== '') {
     Bugsnag.start({
@@ -14,6 +15,12 @@ if (typeof bugsnagKey === 'string' && bugsnagKey !== '') {
         releaseStage: process.env.REACT_APP_RELEASE_STAGE,
         plugins: [new BugsnagPluginReact()],
         enabledReleaseStages: ['production', 'staging', 's2', 's3', 's4', 's5', 'sandbox'],
+        onError: event => {
+            if (bugsnagUser) {
+                const { email, uid, username } = bugsnagUser;
+                event.setUser(uid, email, username);
+            }
+        },
     });
 
     // @ts-expect-error
@@ -30,17 +37,27 @@ interface Props {
     children: ReactNode;
 }
 
+const selectUser = (state: RootState) => {
+    const isLoggedIn = selectUserLoggedIn(state);
+
+    return isLoggedIn ? selectUserInfo(state) : undefined;
+};
+
 export const ErrorBoundary = BugsnagErrorBoundary
     ? ({ children }: Props): ReactElement => {
-          const user = useSelector(selectUserInfo);
+          const user = useSelector(selectUser);
 
           useEffect(() => {
               if (user) {
                   const { email, uid, username } = user;
-                  Bugsnag.setUser(uid, email, username);
+                  bugsnagUser = { uid, email, username };
               } else {
-                  Bugsnag.setUser(); // clear
+                  bugsnagUser = undefined;
               }
+
+              return () => {
+                  bugsnagUser = undefined;
+              };
           }, [user]);
 
           return <BugsnagErrorBoundary>{children}</BugsnagErrorBoundary>;
