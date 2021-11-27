7b3296c7e4aa69db32fab720c44c29cc0e332b5d
mailer21 <metelew@gmail.com>
2021-11-27 18:41:59 +0300

    s1, s6.s-www.lgk.one

---
+++ config/deploy/s6.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+set :public_url, "https://#{ENV.fetch( 'STAGING_SERVER_6' )}/"
+set :build_domain, ENV.fetch( 'STAGING_SERVER_6' )
+set :stage, 's6'
+set :application, -> { 'baseapp-' + fetch(:stage).to_s }
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
+
+server ENV.fetch( 'STAGING_SERVER_6' ), user: fetch(:user), roles: fetch(:roles)
+
+before 'link_env', 'link_auth0'
