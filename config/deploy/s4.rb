ac929f2ffe54920ba3cc93829b9610b4754514ab
mailer21 <metelew@gmail.com>
2021-11-29 23:43:40 +0300

    s2-s5 moved

--- config/deploy/s4.rb
+++ config/deploy/s4.rb
@@ -4,6 +4,7 @@ set :public_url, "https://#{ENV.fetch( 'STAGING_SERVER_4' )}/"
 set :build_domain, ENV.fetch( 'STAGING_SERVER_4' )
 set :stage, 's4'
 set :application, -> { 'baseapp-' + fetch(:stage).to_s }
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
 
 server ENV.fetch( 'STAGING_SERVER_4' ), user: fetch(:user), roles: fetch(:roles)
 
--- config/deploy/s5.rb
+++ config/deploy/s5.rb
@@ -4,6 +4,7 @@ set :public_url, "https://#{ENV.fetch( 'STAGING_SERVER_5' )}/"
 set :build_domain, ENV.fetch( 'STAGING_SERVER_5' )
 set :stage, 's5'
 set :application, -> { 'baseapp-' + fetch(:stage).to_s }
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
 
 server ENV.fetch( 'STAGING_SERVER_5' ), user: fetch(:user), roles: fetch(:roles)
 
