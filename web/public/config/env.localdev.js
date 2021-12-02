b6379327da46aceee9fa92a711ca6437c67a433d
Aleksandr Grigorev <cognize6@gmail.com>
2021-12-02 15:01:43 +0300

    chore: use auth.bitzlato.bz for local development

--- web/public/config/env.localdev.js
+++ web/public/config/env.localdev.js
@@ -7,8 +7,8 @@ window.env = {
     rangerUrl: 'ws://localhost:8080/api/v2/ranger',
   },
   auth0: {
-    domain: 'bitzlato-dev.auth0.com',
-    client_id: 'OL926gD0Zha6h80uJx4TVhJLMKrJemjb',
+    domain: 'auth.bitzlato.bz',
+    client_id: 'sW5Er9tgeD9T8XuklzCX_FcNX0ETttJK',
     redirect_uri: 'http://localhost:8080/success_signin',
     auth_url: 'http://localhost:8080/api/v2/barong/identity/sessions/auth0',
     signedin_url: 'http://localhost:8080/trading',
