f8390d12ca6f27aeae10b4f7001b4f1121eefea8
Aleksandr Grigorev <cognize6@gmail.com>
2021-12-20 16:56:04 +0300

    fix: redirect to autorize from the verification page

--- web/src/screens/VerifyEmail/VerifyEmail.tsx
+++ web/src/screens/VerifyEmail/VerifyEmail.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectUserDataError } from 'src/modules/user/profile/selectors';
-import { userFetch } from 'src/modules/user/profile/actions';
 import { Button } from 'src/components/Button/Button';
 import { Card } from 'src/components/Card/Card';
 import { Box } from 'src/components/Box/Box';
 import { useT } from 'src/hooks/useT';
+import { loginWithRedirect } from 'src/helpers/auth0';
 import s from 'src/containers/QuickExchange/QuickExchange.postcss';
 
 export const VerifyEmailModal: React.FC = () => {
   const t = useT();
-  const dispatch = useDispatch();
   const userError = useSelector(selectUserDataError);
   const email = userError?.payload?.email ?? '';
 
@@ -19,7 +18,7 @@ export const VerifyEmailModal: React.FC = () => {
       <Box as="p" textSize="lg">
         {t('verify.email.content', { email })}
       </Box>
-      <Button size="large" variant="primary" onClick={() => dispatch(userFetch())}>
+      <Button size="large" variant="primary" onClick={() => loginWithRedirect()}>
         OK
       </Button>
     </Card>
