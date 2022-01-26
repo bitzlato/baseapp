0dac25f70f6435b5e5e21c80d2b5feb525d08ad9
Maksim Gerasimov <treizeger@gmail.com>
2022-01-26 12:41:22 +0300

    Update dependecies swagger-ui-react to ^4.3.0

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -58,6 +58,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
     modules: ['node_modules', __dirname],
+    fallback: { stream: require.resolve('stream-browserify') },
   },
 
   module: {
