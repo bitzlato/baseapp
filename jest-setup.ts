500b8058dc34562bc2c0cb75d91d48fa39f86d6e
Maksim Gerasimov <treizeger@gmail.com>
2022-02-01 13:24:34 +0300

    chore: Move eslint to root

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import './web/public/config/env.testing';
+import 'web/public/config/env.testing';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
