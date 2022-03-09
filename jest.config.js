61a47e9624245e8ec7a55dd4768037fa891141c7
Maksim Gerasimov <treizeger@gmail.com>
2022-03-09 17:08:52 +0300

    fix: Fix tests where vanilla-extract is used

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,24 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   verbose: true,
   moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': require.resolve('identity-obj-proxy'),
+    '^.+\\.(less|scss)$': require.resolve('identity-obj-proxy'),
     '\\.(postcss)$': require.resolve('identity-obj-proxy'),
     '^react-scroll-to-component$': require.resolve('identity-obj-proxy'),
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/file.js',
-    '\\.(css|scss|pcss)$': '<rootDir>/__mocks__/style.js',
+    '\\.(scss|pcss)$': '<rootDir>/__mocks__/style.js',
   },
   transform: {
+    '^.+\\.css\\.ts$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          '@babel/preset-typescript',
+        ],
+        plugins: ['@vanilla-extract/babel-plugin'],
+      },
+    ],
     '^.+\\.(jsx?|tsx?)$': [
       '@swc/jest',
       {
