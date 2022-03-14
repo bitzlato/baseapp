f91ad0196529e0199657b707f18d9cd90e8a5146
Maksim Gerasimov <treizeger@gmail.com>
2022-03-14 20:46:21 +0300

    chore: Copy helper can accept HTMLInputElement

--- web/src/helpers/copy.ts
+++ web/src/helpers/copy.ts
@@ -1,5 +1,6 @@
-export const copy = (id: string) => {
-  const copyText: HTMLInputElement | null = document.querySelector(`#${id}`);
+export const copy = (id: string | HTMLInputElement) => {
+  const copyText: HTMLInputElement | null =
+    typeof id === 'string' ? document.querySelector(`#${id}`) : id;
 
   if (copyText) {
     copyText.select();
