d43b65290871b4beb53ba01cde4039e83d4b8d28
Aleksandr Grigorev <cognize6@gmail.com>
2022-03-21 13:43:58 +0300

    feat: redirect to p2p from market logo
    - place Beta label inside the link

--- web/src/components/shared/Header/Logo.tsx
+++ web/src/components/shared/Header/Logo.tsx
@@ -1,4 +1,5 @@
 import { FC, useContext } from 'react';
+import cn from 'classnames';
 import Beta from 'web/src/assets/svg/Beta.svg';
 import { Box } from 'web/src/components/ui/Box';
 
@@ -12,22 +13,14 @@ export const Logo: FC = () => {
     beta = true,
     logoDarkURL,
     logoLightURL,
-    renderLinkComponent,
   } = useContext(HeaderContext);
 
   return (
     <Box position="relative" mr="3x">
-      {renderLinkComponent({
-        to: toMainPage,
-        children: (
-          <img className={s.logo} alt="Logo" src={theme === 'dark' ? logoDarkURL : logoLightURL} />
-        ),
-      })}
-      {beta && (
-        <Box className={s.beta}>
-          <Beta className={s.svg} />
-        </Box>
-      )}
+      <a href={toMainPage}>
+        <img className={s.logo} alt="Logo" src={theme === 'dark' ? logoDarkURL : logoLightURL} />
+        {beta && <Beta className={cn(s.beta, s.svg)} />}
+      </a>
     </Box>
   );
 };
