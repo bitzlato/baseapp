1ad699b697a4f5575a6a5ebf9003c2d5d4fc0ddd
Maksim Gerasimov <treizeger@gmail.com>
2022-03-25 11:08:28 +0300

    fix: Fix negative rating in Profile [#181665816]

--- web/src/helpers/parseAmount.test.ts
+++ web/src/helpers/parseAmount.test.ts
@@ -2,12 +2,12 @@ import { parseAmount } from './parseAmount';
 
 describe('parseAmount', () => {
   test('should parse amount', () => {
-    expect(parseAmount('', 2)).toEqual('');
-    expect(parseAmount('0', 2)).toEqual('0');
-    expect(parseAmount('99', 2)).toEqual('99');
-    expect(parseAmount('99.1234', 2)).toEqual('99.12');
-    expect(parseAmount('99.1234', 0)).toEqual('99');
-    expect(parseAmount('99.1234', 5)).toEqual('99.1234');
-    expect(parseAmount('99.123456789', 8)).toEqual('99.12345678');
+    expect(parseAmount('', { maxFractionDigits: 2 })).toEqual('');
+    expect(parseAmount('0', { maxFractionDigits: 2 })).toEqual('0');
+    expect(parseAmount('99', { maxFractionDigits: 2 })).toEqual('99');
+    expect(parseAmount('99.1234', { maxFractionDigits: 2 })).toEqual('99.12');
+    expect(parseAmount('99.1234', { maxFractionDigits: 0 })).toEqual('99');
+    expect(parseAmount('99.1234', { maxFractionDigits: 5 })).toEqual('99.1234');
+    expect(parseAmount('99.123456789', { maxFractionDigits: 8 })).toEqual('99.12345678');
   });
 });
