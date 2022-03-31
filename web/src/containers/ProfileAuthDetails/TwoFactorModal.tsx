e1df4a46c27618009abfa6d1c73bf7e8d513692f
Maksim Gerasimov <treizeger@gmail.com>
2022-03-31 19:26:30 +0300

    fix: Use ui/Modal in TwoFactorModal

--- web/src/containers/ProfileAuthDetails/TwoFactorModal.tsx
+++ web/src/containers/ProfileAuthDetails/TwoFactorModal.tsx
@@ -3,10 +3,11 @@ import { Box } from 'web/src/components/Box/Box';
 import { Button } from 'web/src/components/ui/Button';
 import { TextInput } from 'web/src/components/Input/TextInput';
 import { useT } from 'web/src/hooks/useT';
-import { Modal2 } from 'web/src/components/Modal/Modal2';
 import { formatSeconds } from 'web/src/helpers/formatSeconds';
 import { useCountdown } from 'web/src/hooks/useCountdown';
 import { isValidCode, OTP_TIMEOUT } from 'web/src/helpers/codeValidation';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'web/src/components/ui/Modal';
+import { AutoFocusInside } from 'react-focus-on';
 
 interface Props {
   onClose: () => void;
@@ -38,22 +39,28 @@ export const TwoFactorModal: FC<Props> = ({ onClose, onSend, buttonText, text })
   };
 
   return (
-    <Modal2 header={t('2FA Verification')} onClose={onClose} show>
-      <Box col spacing="3">
-        {text}
-        <TextInput
-          label={t('2FA code')}
-          placeholder={!text ? t('Enter 2FA code from the app') : undefined}
-          value={code}
-          onChange={setCode}
-          onKeyPress={handleKeyPress}
-          maxLength={6}
-          autoFocus
-        />
-        <Button disabled={disabled} onClick={handleSend}>
+    <Modal onClose={onClose} show>
+      <ModalHeader>{t('2FA Verification')}</ModalHeader>
+      <ModalBody>
+        <Box col spacing="3">
+          {text}
+          <AutoFocusInside>
+            <TextInput
+              label={t('2FA code')}
+              placeholder={!text ? t('Enter 2FA code from the app') : undefined}
+              value={code}
+              onChange={setCode}
+              onKeyPress={handleKeyPress}
+              maxLength={6}
+            />
+          </AutoFocusInside>
+        </Box>
+      </ModalBody>
+      <ModalFooter>
+        <Button fullWidth disabled={disabled} onClick={handleSend}>
           {countdown > 0 ? formatSeconds(countdown) : buttonText ?? t('Send')}
         </Button>
-      </Box>
-    </Modal2>
+      </ModalFooter>
+    </Modal>
   );
 };
