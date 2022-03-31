df09dcf01b4a5b593c2f3f1c359bbb81b6bb9312
Wiktor Serow <vikseriq@gmail.com>
2022-03-31 19:46:45 +0200

    feat: #379 hot module reload (#450)
    * feat: #379 integrate react-refresh-webpack-plugin
    
    feat: upgrade webpack and configure custom version of plugin
    fix: disable DocumentationScreen to resolve ModuleFederationPlugin issues
    
    * fix: rollback Documentation screen
    fix: track new packages inside .yarn/cache

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -10,6 +10,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const StatoscopeWebpackPlugin = require('@statoscope/webpack-plugin').default;
 const { BugsnagSourceMapUploaderPlugin } = require('webpack-bugsnag-plugins');
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const deps = require('./package.json').dependencies;
 
@@ -190,6 +191,7 @@ module.exports = {
   },
 
   optimization: {
+    runtimeChunk: 'single',
     moduleIds: isDevelopment ? 'named' : 'deterministic',
     chunkIds: isDevelopment ? 'named' : 'deterministic',
     minimize: !isDevelopment,
@@ -238,6 +240,8 @@ module.exports = {
       AUTH0_DOMAIN: null,
       AUTH0_CLIENT_ID: null,
     }),
+
+    isDevelopment && new ReactRefreshPlugin(),
 
     new webpack.IgnorePlugin({
       resourceRegExp: /^\.\/locale$/,
@@ -296,6 +300,7 @@ module.exports = {
                   proxyReq.removeHeader('origin');
                   proxyReq.setHeader('origin', `https://${process.env.PROXY_HOST}`);
                   socket.on('error', (error) => {
+                    // eslint-disable-next-line no-console
                     console.error(error);
                   });
                 },
