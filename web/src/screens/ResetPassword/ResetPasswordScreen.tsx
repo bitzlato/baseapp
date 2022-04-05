3ac8ca8bd889f706a773d31b0b70d3ac84eb67c2
Aleksandr Grigorev <cognize6@gmail.com>
2022-04-05 11:06:39 +0300

    chore: add new Reset Password form

---
+++ web/src/screens/ResetPassword/ResetPasswordScreen.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { Container } from 'web/src/components/Container/Container';
+import { Card } from 'web/src/components/Card/Card';
+import { ResetPassword } from 'web/src/containers/ResetPassword/ResetPassword';
+
+export const ResetPasswordScreen: FC = () => {
+  return (
+    <Container maxWidth="sm" my="4">
+      <Card>
+        <ResetPassword />
+      </Card>
+    </Container>
+  );
+};
