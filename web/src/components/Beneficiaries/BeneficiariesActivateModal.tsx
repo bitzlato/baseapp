7f17288d15b0ef594b5461670c8a507e60e3c2e9
YuriFA <yurik45gb@gmail.com>
2022-04-25 20:05:49 +0300

    fix: Refactor Beneficiaries active and fail modals [#181969697]

--- web/src/components/Beneficiaries/BeneficiariesActivateModal.tsx
+++ web/src/components/Beneficiaries/BeneficiariesActivateModal.tsx
@@ -1,48 +1,49 @@
-import * as React from 'react';
-import cn from 'classnames';
-import { Button } from 'react-bootstrap';
-import { useIntl } from 'react-intl';
+import { FC, useState, useCallback, KeyboardEvent, memo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useT } from 'web/src/hooks/useT';
 import { formatSeconds } from 'web/src/helpers/formatSeconds';
 import { useCountdown } from 'web/src/hooks/useCountdown';
-import { LetterIcon } from '../../assets/images/LetterIcon';
-import { MobileModal } from '../../mobile/components/Modal';
+import { LetterIcon } from 'web/src/assets/images/LetterIcon';
 import {
   beneficiariesActivate,
   beneficiariesResendPin,
   Beneficiary,
   selectMobileDeviceState,
-} from '../../modules';
-import { Box } from '../Box/Box';
-import { TextInput } from '../Input/TextInput';
+} from 'web/src/modules';
+import { TextInput } from 'web/src/components/Input/TextInput';
 import { isValidCode, PIN_TIMEOUT } from 'web/src/helpers/codeValidation';
+import { Text } from 'web/src/components/ui/Text';
+import { Box } from 'web/src/components/ui/Box';
+import { Button } from 'web/src/components/ui/Button';
+import { Modal, ModalHeader } from 'web/src/components/ui/Modal';
 
 interface Props {
   beneficiariesAddData: Beneficiary;
   handleToggleConfirmationModal: () => void;
 }
 
-const BeneficiariesActivateModalComponent: React.FC<Props> = (props: Props) => {
-  const [code, setCode] = React.useState('');
+const BeneficiariesActivateModalComponent: FC<Props> = ({
+  handleToggleConfirmationModal,
+  beneficiariesAddData,
+}: Props) => {
+  const [code, setCode] = useState('');
 
-  const { formatMessage } = useIntl();
+  const t = useT();
   const dispatch = useDispatch();
 
   const isMobileDevice = useSelector(selectMobileDeviceState);
 
   const { start, countdown } = useCountdown();
 
-  const { handleToggleConfirmationModal, beneficiariesAddData } = props;
-
-  const handleChangeFieldValue = React.useCallback((value: string) => {
+  const handleChangeFieldValue = useCallback((value: string) => {
     setCode(value);
   }, []);
 
-  const handleClearModalsInputs = React.useCallback(() => {
+  const handleClearModalsInputs = useCallback(() => {
     setCode('');
   }, []);
 
-  const handleClickToggleConfirmationModal = React.useCallback(
+  const handleClickToggleConfirmationModal = useCallback(
     (clear?: boolean) => () => {
       handleToggleConfirmationModal();
 
@@ -53,7 +54,7 @@ const BeneficiariesActivateModalComponent: React.FC<Props> = (props: Props) => {
     [handleToggleConfirmationModal, handleClearModalsInputs],
   );
 
-  const handleSubmitConfirmationModal = React.useCallback(() => {
+  const handleSubmitConfirmationModal = useCallback(() => {
     if (beneficiariesAddData) {
       start(PIN_TIMEOUT);
       dispatch(
@@ -66,14 +67,14 @@ const BeneficiariesActivateModalComponent: React.FC<Props> = (props: Props) => {
     }
   }, [code, dispatch, beneficiariesAddData, handleClearModalsInputs]);
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter' && isValidCode(code) && countdown < 1) {
       event.preventDefault();
       handleSubmitConfirmationModal();
     }
   };
 
-  const handleResendConfirmationCode = React.useCallback(() => {
+  const handleResendConfirmationCode = useCallback(() => {
     if (beneficiariesAddData) {
       dispatch(
         beneficiariesResendPin({
@@ -83,96 +84,67 @@ const BeneficiariesActivateModalComponent: React.FC<Props> = (props: Props) => {
     }
   }, [beneficiariesAddData, dispatch]);
 
-  const renderConfirmationModalBody = React.useCallback(() => {
-    const isDisabled = !isValidCode(code) || countdown > 0;
+  const isDisabled = !isValidCode(code) || countdown > 0;
 
-    return (
-      <div className="cr-email-form__form-content">
-        <div className="confirmation-modal__content">
-          <LetterIcon className="confirmation-modal__content__icon" />
-          <span className="confirmation-modal__content__text">
-            {formatMessage({ id: 'page.body.wallets.beneficiaries.confirmationModal.body.text' })}
-          </span>
-        </div>
-        <Box col spacing="2">
+  return (
+    <Modal size="lg" show onClose={handleClickToggleConfirmationModal(true)}>
+      <ModalHeader>{t('page.body.wallets.beneficiaries.confirmationModal.header')}</ModalHeader>
+      <Box
+        fontSize="medium"
+        mx="6x"
+        py="4x"
+        borderTopWidth="1x"
+        borderColor="modalHeaderBorderBottom"
+        borderTopStyle="solid"
+      >
+        <Box display="flex" flexDirection="row">
+          {isMobileDevice ? null : (
+            <Box mr="4x" flexShrink={0}>
+              <LetterIcon />
+            </Box>
+          )}
+          <Text color={isMobileDevice ? 'text' : 'secondary'}>
+            {t('page.body.wallets.beneficiaries.confirmationModal.body.text')}
+          </Text>
+        </Box>
+        <Box display="block" mt="6x">
           <TextInput
-            label={formatMessage({
-              id: 'page.body.wallets.beneficiaries.confirmationModal.body.confirmationModalCode',
-            })}
+            label={t(
+              'page.body.wallets.beneficiaries.confirmationModal.body.confirmationModalCode',
+            )}
+            autoFocus
             labelVisible
             value={code}
             onChange={handleChangeFieldValue}
             onKeyPress={handleKeyPress}
-            autoFocus
           />
-          <div className="cr-email-form__button-wrapper cr-email-form__button-wrapper--double">
-            <Button onClick={handleResendConfirmationCode} size="lg" variant="secondary">
-              {formatMessage({
-                id: 'page.body.wallets.beneficiaries.confirmationModal.body.resendButton',
-              })}
-            </Button>
+          <Box display="flex" alignItems="center" mt="4x">
             <Button
+              color="secondary"
+              variant="outlined"
+              fullWidth
+              onClick={handleResendConfirmationCode}
+            >
+              {t('page.body.wallets.beneficiaries.confirmationModal.body.resendButton')}
+            </Button>
+            <Box w="4x" flexShrink={0} />
+            <Button
+              color="primary"
+              fullWidth
               disabled={isDisabled}
               onClick={handleSubmitConfirmationModal}
-              size="lg"
-              variant="primary"
             >
               {countdown
                 ? formatSeconds(countdown)
-                : formatMessage({
-                    id: 'page.body.wallets.beneficiaries.confirmationModal.body.button',
-                  })}
+                : t('page.body.wallets.beneficiaries.confirmationModal.body.button')}
             </Button>
-          </div>
+          </Box>
         </Box>
-      </div>
-    );
-  }, [code, countdown, formatMessage, handleResendConfirmationCode, handleSubmitConfirmationModal]);
-
-  const renderConfirmationModalHeader = React.useCallback(() => {
-    return (
-      <div className="cr-email-form__options-group">
-        <div className="cr-email-form__option">
-          <div className="cr-email-form__option-inner">
-            {formatMessage({ id: 'page.body.wallets.beneficiaries.confirmationModal.header' })}
-            <span
-              className="pg-profile-page__close pg-profile-page__pull-right"
-              onClick={handleClickToggleConfirmationModal(true)}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }, [handleClickToggleConfirmationModal, formatMessage]);
-
-  const renderContent = React.useCallback(() => {
-    const className = cn('beneficiaries-confirmation-modal', {
-      'cr-modal': !isMobileDevice,
-    });
-
-    return (
-      <div className={className}>
-        <div className="cr-email-form">
-          {renderConfirmationModalHeader()}
-          {renderConfirmationModalBody()}
-        </div>
-      </div>
-    );
-  }, [isMobileDevice, renderConfirmationModalBody, renderConfirmationModalHeader]);
-
-  return isMobileDevice ? (
-    <MobileModal
-      onClose={props.handleToggleConfirmationModal}
-      title={formatMessage({ id: 'page.mobile.wallet.withdraw.modal.new.account' })}
-      isOpen
-    >
-      {renderContent()}
-    </MobileModal>
-  ) : (
-    renderContent()
+      </Box>
+    </Modal>
   );
 };
 
-const BeneficiariesActivateModal = React.memo(BeneficiariesActivateModalComponent);
+const BeneficiariesActivateModal = memo(BeneficiariesActivateModalComponent);
 
 export { BeneficiariesActivateModal };
