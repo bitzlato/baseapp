511cbbfb0eee57b856be38c931744120d0085949
Maksim Gerasimov <treizeger@gmail.com>
2022-04-25 13:05:37 +0300

    chore: Update AppContext

--- web/src/components/app/AppContext.tsx
+++ web/src/components/app/AppContext.tsx
@@ -1,9 +1,11 @@
 import { createContext, useContext } from 'react';
+import { User } from 'web/src/modules/user/profile/types';
 import { Language, Theme } from 'web/src/types';
 
 interface AppContextValue {
   theme: Theme;
   lang: Language;
+  user?: User;
 }
 
 export const AppContext = createContext(null as any as AppContextValue);
@@ -11,3 +13,7 @@ export const AppContext = createContext(null as any as AppContextValue);
 export const useAppContext = () => useContext(AppContext);
 
 export const useTheme = () => useAppContext().theme;
+
+export const useLanguage = () => useAppContext().lang;
+
+export const useUser = () => useAppContext().user;
--- web/src/components/app/AppContextProvider.tsx
+++ web/src/components/app/AppContextProvider.tsx
@@ -1,12 +1,13 @@
 import { FC, useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { selectCurrentColorTheme, selectCurrentLanguage } from 'web/src/modules';
+import { selectCurrentColorTheme, selectCurrentLanguage, selectUserInfo } from 'web/src/modules';
 import { AppContext } from './AppContext';
 
 export const AppContextProvider: FC = ({ children }) => {
   const theme = useSelector(selectCurrentColorTheme);
   const lang = useSelector(selectCurrentLanguage);
-  const value = useMemo(() => ({ theme, lang }), [theme, lang]);
+  const user = useSelector(selectUserInfo);
+  const value = useMemo(() => ({ theme, lang, user }), [theme, lang, user]);
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
