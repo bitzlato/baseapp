0fb081fd5b30a143370e953fe902efe76e091136
Maksim Gerasimov <treizeger@gmail.com>
2022-04-25 13:07:49 +0300

    chore: Add hooks to get a currency object

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,3 +1,4 @@
+import { Currency } from '@bitzlato/money-js';
 import { WrappedComponentProps } from 'react-intl';
 
 export type IntlProps = WrappedComponentProps;
@@ -8,3 +9,8 @@ export type Theme = 'light' | 'dark';
 export type OptionalWithUndefined<T> = {
   [P in keyof T]: Pick<T, P> extends Required<Pick<T, P>> ? T[P] : T[P] | undefined;
 };
+
+export interface MoneyCurrency extends Currency {
+  name: string;
+  sign: string;
+}
