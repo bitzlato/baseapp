0b3cf2b62fd3e481c5f6c326f61e4e834987a646
Maksim Gerasimov <treizeger@gmail.com>
2022-04-25 13:07:49 +0300

    chore: Add hooks to get a currency object

--- web/src/hooks/data/useFetchP2PCurrencies.ts
+++ web/src/hooks/data/useFetchP2PCurrencies.ts
@@ -1,6 +1,7 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { p2pUrl } from 'web/src/api/config';
 import { P2PCurrencies, P2PCurrencyOption } from 'web/src/modules/public/currencies/types';
+import { MoneyCurrency } from 'web/src/types';
 import { useFetch, FetchResponse } from './useFetch';
 
 export const useFetchP2PCurrencies = () => {
@@ -24,4 +25,45 @@ export const useP2PCurrencyOptions = (): FetchResponse<P2PCurrencyOption[]> => {
   }, [data]);
 
   return { data: options, error };
+};
+
+export const useFiatCurrencies = (): {
+  getFiatCurrency: (code: string) => MoneyCurrency;
+  fiatCurrencies: Record<string, MoneyCurrency>;
+} => {
+  const { data } = useFetchP2PCurrencies();
+
+  const fiatCurrencies = useMemo(() => {
+    if (data) {
+      return data.reduce<Record<string, MoneyCurrency>>((acc, P2PCurrency) => {
+        acc[P2PCurrency.code] = {
+          code: P2PCurrency.code,
+          name: P2PCurrency.name,
+          sign: P2PCurrency.sign,
+          minorUnit: 2,
+        };
+
+        return acc;
+      }, {});
+    }
+
+    return {};
+  }, [data]);
+  const getFiatCurrency = useCallback(
+    (code: string): MoneyCurrency => {
+      const maybeMoneyCurrency = fiatCurrencies[code];
+
+      return (
+        maybeMoneyCurrency ?? {
+          code,
+          name: code,
+          sign: code,
+          minorUnit: 2,
+        }
+      );
+    },
+    [fiatCurrencies],
+  );
+
+  return { getFiatCurrency, fiatCurrencies };
 };
