0a5dba488c9747a6bd1c84258ff105f7c30ddb09
Maksim Gerasimov <treizeger@gmail.com>
2022-04-25 13:05:37 +0300

    chore: Update AppContext
    # Conflicts:
    #	web/src/components/app/AppContext.tsx
    #	web/src/components/app/AppContextProvider.tsx

--- web/src/components/app/AppContextProvider.tsx
+++ web/src/components/app/AppContextProvider.tsx
@@ -1,11 +1,13 @@
 import { FC, useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { selectCurrentColorTheme } from 'web/src/modules';
+import { selectCurrentColorTheme, selectCurrentLanguage, selectUserInfo } from 'web/src/modules';
 import { AppContext } from './AppContext';
 
 export const AppContextProvider: FC = ({ children }) => {
   const theme = useSelector(selectCurrentColorTheme);
-  const value = useMemo(() => ({ theme }), [theme]);
+  const lang = useSelector(selectCurrentLanguage);
+  const user = useSelector(selectUserInfo);
+  const value = useMemo(() => ({ theme, lang, user }), [theme, lang, user]);
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
