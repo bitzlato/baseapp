a9c449d09319107ee8b027887fb5c84052f1e0ba
YuriFA <yurik45gb@gmail.com>
2022-04-25 20:05:49 +0300

    fix: Refactor Beneficiaries active and fail modals [#181969697]

--- web/src/components/Beneficiaries/BeneficiariesFailAddModal.tsx
+++ web/src/components/Beneficiaries/BeneficiariesFailAddModal.tsx
@@ -1,82 +1,45 @@
-import classnames from 'classnames';
-import * as React from 'react';
-import { Button } from 'react-bootstrap';
-import { useIntl } from 'react-intl';
+import { FC, memo } from 'react';
 import { Link } from 'react-router-dom';
-import { MobileModal } from '../../mobile/components/Modal';
+import { useT } from 'web/src/hooks/useT';
+import { Modal, ModalHeader } from 'web/src/components/ui/Modal';
+import { Text } from 'web/src/components/ui/Text';
+import { Box } from 'web/src/components/ui/Box';
+import { Button } from 'web/src/components/ui/Button';
 
 interface Props {
   handleToggleFailModal: () => void;
   isMobileDevice?: boolean;
 }
 
-const BeneficiariesFailAddModalComponent: React.FC<Props> = (props: Props) => {
-  const { formatMessage } = useIntl();
-  const { handleToggleFailModal, isMobileDevice } = props;
+const BeneficiariesFailAddModalComponent: FC<Props> = ({ handleToggleFailModal }: Props) => {
+  const t = useT();
 
-  const ModalHeader = React.useCallback(
-    () => (
-      <div className="cr-email-form__options-group">
-        <div className="cr-email-form__option">
-          <div className="cr-email-form__option-inner">
-            {formatMessage({ id: 'page.body.wallets.beneficiaries.failAddModal.header' })}
-            <span
-              className="pg-profile-page__close pg-profile-page__pull-right"
-              onClick={handleToggleFailModal}
-            />
-          </div>
-        </div>
-      </div>
-    ),
-    [handleToggleFailModal, formatMessage],
-  );
-
-  const ModalBody = React.useCallback(
-    () => (
-      <div className="cr-email-form__form-content">
-        <span className="cr-email-form__form-content__info">
-          {formatMessage({ id: 'page.body.wallets.beneficiaries.failAddModal.content' })}
-        </span>
-        <div className="cr-email-form__button-wrapper">
+  return (
+    <Modal size="lg" show onClose={handleToggleFailModal}>
+      <ModalHeader>{t('page.body.wallets.beneficiaries.failAddModal.header')}</ModalHeader>
+      <Box
+        fontSize="medium"
+        mx="6x"
+        py="4x"
+        borderTopWidth="1x"
+        borderColor="modalHeaderBorderBottom"
+        borderTopStyle="solid"
+      >
+        <Text>
+          {t('page.body.wallets.beneficiaries.failAddModal.content')}
+        </Text>
+        <Box mt="6x">
           <Link to="/confirm">
-            <Button size="lg" variant="primary" className="cr-email-form__button-wrapper__btn">
-              {formatMessage({ id: 'page.body.wallets.beneficiaries.failAddModal.button' })}
+            <Button color="primary" fullWidth>
+              {t('page.body.wallets.beneficiaries.failAddModal.button')}
             </Button>
           </Link>
-        </div>
-      </div>
-    ),
-    [formatMessage],
-  );
-
-  const renderContent = React.useCallback(() => {
-    const className = classnames('beneficiaries-fail-modal', {
-      'cr-modal': !isMobileDevice,
-    });
-
-    return (
-      <div className={className}>
-        <div className="cr-email-form">
-          <ModalHeader />
-          <ModalBody />
-        </div>
-      </div>
-    );
-  }, [isMobileDevice]);
-
-  return props.isMobileDevice ? (
-    <MobileModal
-      isOpen
-      onClose={props.handleToggleFailModal}
-      title={formatMessage({ id: 'page.body.wallets.beneficiaries.failAddModal.content' })}
-    >
-      {renderContent()}
-    </MobileModal>
-  ) : (
-    renderContent()
+        </Box>
+      </Box>
+    </Modal>
   );
 };
 
-const BeneficiariesFailAddModal = React.memo(BeneficiariesFailAddModalComponent);
+const BeneficiariesFailAddModal = memo(BeneficiariesFailAddModalComponent);
 
 export { BeneficiariesFailAddModal };
