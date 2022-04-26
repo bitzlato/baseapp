47cfdc3f5515854bad6afae8c44a8655e576b49f
Maksim Gerasimov <treizeger@gmail.com>
2022-04-26 22:46:02 +0300

    fix: Fix button style in P2P api keys

--- web/src/components/profile/apiKeys/P2PPublicApiKeyModal.tsx
+++ web/src/components/profile/apiKeys/P2PPublicApiKeyModal.tsx
@@ -3,7 +3,6 @@ import { Button } from 'web/src/components/ui/Button';
 import { useT } from 'web/src/hooks/useT';
 import { Modal, ModalFooter, ModalHeader, ModalBody } from 'web/src/components/ui/Modal';
 import { Box } from 'web/src/components/ui/Box';
-import { IconButton } from 'web/src/components/IconButton/IconButton';
 import CopyLinealIcon from 'web/src/assets/svg/CopyLinealIcon.svg';
 import { CopyField } from 'web/src/components/ui/CopyField';
 
@@ -20,12 +19,12 @@ export const P2PPublicApiKeyModal: FC<Props> = ({ publicKey }) => {
 
   return (
     <>
-      <Box as={IconButton} display="flex" alignItems="center" onClick={handleClickOpen}>
+      <Button variant="text" color="clarified" size="small" onClick={handleClickOpen}>
         <Box as="span" mr="2x">
           <CopyLinealIcon width="16" height="16" />
         </Box>
         {t('page.body.profile.apiKeys.modal.btn.show')}
-      </Box>
+      </Button>
       <Modal size="lg" show={open} onClose={handleClose}>
         <ModalHeader>{t('Public key')}</ModalHeader>
         <ModalBody>
