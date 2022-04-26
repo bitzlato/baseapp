7a9c106bba3461260d1f2a60be62c315d159189b
Maksim Gerasimov <treizeger@gmail.com>
2022-04-26 22:07:25 +0300

    fix: Remove unnecessary processing of the avatar link

--- web/src/components/profile/UserAvatar.tsx
+++ web/src/components/profile/UserAvatar.tsx
@@ -9,31 +9,24 @@ interface UserAvatarProps {
   isLoading?: boolean;
 }
 
-export const UserAvatar: FC<UserAvatarProps> = ({ image, isLoading }) => {
-  const re = /avatar\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]+)/g;
-  const result = re.exec(image);
-  const avatarUrl =
-    result && result?.length > 0 ? `https://static.bitzlato.com/avatars/${result[1]}.jpg` : image;
-
-  return (
-    <Box
-      position="relative"
-      borderRadius="2x"
-      borderWidth="1x"
-      borderColor="modalHeaderBorderBottom"
-      borderStyle="solid"
-      className={s.avatarBox}
-    >
-      {isLoading && (
-        <Box position="absolute" className={s.avatarSpinner}>
-          <Spinner />
-        </Box>
-      )}
-      {image !== '' ? (
-        <img src={avatarUrl} alt="Profile avatar" className={s.avatarImage} />
-      ) : (
-        <DefaultAvatar />
-      )}
-    </Box>
-  );
-};
+export const UserAvatar: FC<UserAvatarProps> = ({ image, isLoading }) => (
+  <Box
+    position="relative"
+    borderRadius="2x"
+    borderWidth="1x"
+    borderColor="modalHeaderBorderBottom"
+    borderStyle="solid"
+    className={s.avatarBox}
+  >
+    {isLoading && (
+      <Box position="absolute" className={s.avatarSpinner}>
+        <Spinner />
+      </Box>
+    )}
+    {image !== '' ? (
+      <img src={image} alt="Profile avatar" className={s.avatarImage} />
+    ) : (
+      <DefaultAvatar />
+    )}
+  </Box>
+);
