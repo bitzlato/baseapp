96996fa1dbad6763aed9077094d0a550941bcc72
Aleksandr Grigorev <cognize6@gmail.com>
2022-05-13 13:28:42 +0300

    chore: add P2PFiatMoney component [#181997699]

--- web/src/components/AmountFormat/getFormatOptionsByLanguage.tsx
+++ web/src/components/AmountFormat/getFormatOptionsByLanguage.tsx
@@ -16,3 +16,17 @@ export const getFormatOptionsByLanguage = (language: Language): FormatOptions =>
       };
   }
 };
+
+const CRYPTO_CURRENCY_CODES_WITH_CENTS = new Set(['MCR', 'USDT', 'USDC', 'DOGE', 'DAI', 'MDT']);
+
+export const getP2PFiatOptionsByCode = (code: string): FormatOptions => {
+  return CRYPTO_CURRENCY_CODES_WITH_CENTS.has(code)
+    ? {
+        minFractionDigits: 0,
+        maxFractionDigits: 2,
+      }
+    : {
+        minFractionDigits: 0,
+        maxFractionDigits: 0,
+      };
+};
