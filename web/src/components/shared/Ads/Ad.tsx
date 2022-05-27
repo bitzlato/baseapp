a34a5bf3ce85a8f7012fde071105aea7310ec025
Aleksandr Grigorev <cognize6@gmail.com>
2022-05-27 15:54:12 +0300

    feat: Add mobile view for the advert screen [#181997699]

--- web/src/components/shared/Ads/Ad.tsx
+++ web/src/components/shared/Ads/Ad.tsx
@@ -14,7 +14,7 @@ import VerifiedIcon from 'web/src/assets/svg/VerifiedIcon.svg';
 import { useChangeTrust, useFetchUser } from 'web/src/hooks/data/useFetchP2PUser';
 import { Tooltip } from 'web/src/components/ui/Tooltip';
 import { Button } from 'web/src/components/ui/Button';
-import { useLanguage } from 'web/src/components/app/AppContext';
+import { useAppContext } from 'web/src/components/app/AppContext';
 import { MoneyInput } from 'web/src/components/TextInputCustom/MoneyInputCustom';
 import { P2PFiatFormat } from 'web/src/components/money/P2PFiatFormat';
 import { useFiatCurrencies } from 'web/src/hooks/data/useFetchP2PCurrencies';
@@ -24,22 +24,25 @@ import { P2PMoneyFormat } from 'web/src/components/money/P2PFiatMoney';
 import { parseNumeric } from 'web/src/helpers/parseNumeric';
 import { DealStat } from 'web/src/modules/p2p/user.types';
 import { TrustedButton } from 'web/src/components/traderInfo/TrustedButton';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'web/src/components/ui/Modal';
+import { themeDark } from 'web/src/theme/vars.css';
 import { OnlineStatusByLastActivity } from './OnlineStatus';
+import { AdStat } from './AdStat';
 
 interface UrlParams {
   type: 'buy' | 'sell';
   id: string;
 }
 
-interface Props {}
-
-export const Ad: FC<Props> = () => {
+export const Ad: FC = () => {
   const [from, setFrom] = useState('');
   const [to, setTo] = useState('');
 
+  const [show, setShow] = useState(false);
+
   const t = useSharedT();
   const params = useParams<UrlParams>();
-  const lang = useLanguage();
+  const { lang, isMobileDevice } = useAppContext();
   const { data: advert } = useFetchAdvert(params.id);
   const { data: paymethod } = useFetchPaymethod(advert?.paymethod, lang);
   const { data: owner } = useFetchUser(advert?.owner);
@@ -56,14 +59,6 @@ export const Ad: FC<Props> = () => {
   }
 
   const isBuy = advert.type === 'purchase';
-
-  const tValues = {
-    currency: paymethod.currency,
-    cryptocurrency: advert.cryptocurrency,
-    paymethod: paymethod.description,
-    username: advert.owner,
-  };
-
   const fiatCcy = getFiatCurrency(paymethod.currency);
   const cryptoCcy = getCryptoCurrency(advert.cryptocurrency);
   const rate = createMoney(advert.rate, fiatCcy);
@@ -87,6 +82,198 @@ export const Ad: FC<Props> = () => {
     changeTrust(owner.name, { trust: !owner.trusted });
   };
 
+  const handleClickStart = () => {};
+
+  const handleClickStartMobile = () => {
+    setShow(true);
+  };
+
+  const handleClickCancelMobile = () => {
+    setShow(false);
+  };
+
+  const traderEl = (
+    <Box>
+      <Box display="flex" alignItems="center" gap="2x">
+        <Box as="p" fontSize="lead24">
+          {advert.owner}
+        </Box>
+        {owner.verification && (
+          <Tooltip label={t('Verified user')} placement="top">
+            <div>
+              <Box as={VerifiedIcon} display="block" />
+            </div>
+          </Tooltip>
+        )}
+      </Box>
+      <OnlineStatusByLastActivity lastActivity={owner.lastActivity} />
+    </Box>
+  );
+
+  const labelColor = isMobileDevice ? 'text' : undefined;
+  const traderInfoEl = (
+    <>
+      <AdStat labelColor={labelColor} label={t('Rating')}>
+        <Box display="flex" alignItems="center" gap="5x">
+          <Box display="flex" alignItems="center" gap="1x">
+            <LikeIcon />
+            <span>{owner.feedbacks.find((v) => v.type === 'thumb_up')?.count ?? 0}</span>
+          </Box>
+          <Box display="flex" alignItems="center" gap="1x">
+            <UnLikeIcon />
+            <span>{owner.feedbacks.find((v) => v.type === 'hankey')?.count ?? 0}</span>
+          </Box>
+          <span>{owner.rating}</span>
+        </Box>
+      </AdStat>
+      <AdStat labelColor={labelColor} label={t('Reputation')}>
+        {owner.safetyIndex}
+      </AdStat>
+      <AdStat labelColor={labelColor} label={t('Successful deals')}>
+        {deals.successDeals}
+      </AdStat>
+      <AdStat labelColor={labelColor} label={t('Canceled deals')}>
+        {deals.canceledDeals}
+      </AdStat>
+    </>
+  );
+
+  const termsEl = (
+    <Box p="6x" backgroundColor="adBg" borderRadius="1.5x">
+      <Text variant="title">{t('Trade terms')}</Text>
+      <Box marginTop="6x" whiteSpace="pre-line">
+        {advert.terms ? (
+          <Text>{advert.terms}</Text>
+        ) : (
+          <Text
+            color="secondary"
+            textTransform="uppercase"
+            fontWeight="strong"
+            fontSize="medium"
+            textAlign="center"
+          >
+            {t('Missing.plural')}
+          </Text>
+        )}
+      </Box>
+    </Box>
+  );
+
+  const dealInfoEl = (
+    <>
+      <Text variant="h6">{t('Deal info')}</Text>
+      <AdStat label={t('Rate')}>
+        <P2PMoneyFormat money={rate} cryptoCurrency={cryptoCcy} />
+      </AdStat>
+      <AdStat label={t('Limits')}>
+        <span>
+          <P2PFiatFormat money={min} cryptoCurrency={cryptoCcy} /> —{' '}
+          <P2PMoneyFormat money={max} cryptoCurrency={cryptoCcy} />
+        </span>
+      </AdStat>
+      <AdStat label={t('Bitzlato fee')}>0%</AdStat>
+    </>
+  );
+
+  const inputsEl = (
+    <>
+      <MoneyInput
+        currency={fromCcy.code}
+        value={from}
+        label={t('You pay')}
+        onChange={handleChangeFrom}
+        autoFocus
+      />
+      <MoneyInput
+        currency={toCcy.code}
+        value={to}
+        label={t('You receive')}
+        onChange={handleChangeTo}
+      />
+    </>
+  );
+
+  const tValues = {
+    currency: paymethod.currency,
+    cryptocurrency: advert.cryptocurrency,
+    paymethod: paymethod.description,
+    username: advert.owner,
+  };
+  const header = isBuy ? t('ad.buy', tValues) : t('ad.sell', tValues);
+
+  if (isMobileDevice) {
+    return (
+      <Box display="flex" flexDirection="column" width="full" color="btnPrimaryText">
+        <Box className={themeDark} p="6x" backgroundColor="adTrade">
+          <Box as="h2" fontSize="lead" fontWeight="strong">
+            {header}
+          </Box>
+          <Box mt="5x" bg="paginationItemBgHover" borderRadius="1.5x" px="5x" py="4x">
+            <Box as="h6" fontSize="caption">
+              {t('Trader')}
+            </Box>
+            {traderEl}
+          </Box>
+          <Box
+            mt="3x"
+            bg="paginationItemBgHover"
+            borderRadius="1.5x"
+            px="5x"
+            py="4x"
+            display="flex"
+            flexDirection="column"
+            gap="4x"
+            fontSize="medium"
+          >
+            {traderInfoEl}
+          </Box>
+          <Box mt="5x">
+            <Button fullWidth onClick={handleClickStartMobile}>
+              {t('Start trade')}
+            </Button>
+          </Box>
+        </Box>
+        <Box p="5x" display="flex" flexDirection="column" gap="4x">
+          {termsEl}
+          <Box
+            p="6x"
+            backgroundColor="adBg"
+            borderRadius="1.5x"
+            display="flex"
+            flexDirection="column"
+            gap="3x"
+          >
+            {dealInfoEl}
+          </Box>
+          <Box p="6x" backgroundColor="adBg" borderRadius="1.5x">
+            {t('ad.trade.info')}
+          </Box>
+        </Box>
+        <Modal show={show} onClose={handleClickCancelMobile}>
+          <ModalHeader>{t('Start trade')}</ModalHeader>
+          <ModalBody>
+            <Box display="flex" flexDirection="column" gap="3x">
+              {dealInfoEl}
+            </Box>
+            <Box mt="7x" flex={1} display="flex" flexDirection="column" gap="4x">
+              {inputsEl}
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Box flexGrow={1} display="flex" flexDirection="column" gap="4x">
+              <Button color="secondary" onClick={handleClickStart}>
+                {t('Confirm')}
+              </Button>
+              <Button variant="outlined" color="secondary" onClick={handleClickCancelMobile}>
+                {t('Cancel')}
+              </Button>
+            </Box>
+          </ModalFooter>
+        </Modal>
+      </Box>
+    );
+  }
+
   return (
     <Container maxWidth="xl">
       <Box
@@ -98,25 +285,14 @@ export const Ad: FC<Props> = () => {
         overflow="hidden"
         color="text"
       >
-        <Box p="6x" backgroundColor="adTrade" color="btnPrimaryText">
+        <Box className={themeDark} p="6x" backgroundColor="adTrade" color="btnPrimaryText">
           <Box as="h2" fontSize="lead30">
-            {isBuy ? t('ad.buy', tValues) : t('ad.sell', tValues)}
+            {header}
           </Box>
           <Box mt="6x" display="flex" gap="6x">
             <Box flex={1} display="flex" flexDirection="column" gap="4x">
-              <MoneyInput
-                currency={fromCcy.code}
-                value={from}
-                label={t('You pay')}
-                onChange={handleChangeFrom}
-              />
-              <MoneyInput
-                currency={toCcy.code}
-                value={to}
-                label={t('You receive')}
-                onChange={handleChangeTo}
-              />
-              <Button>{t('Start trade')}</Button>
+              {inputsEl}
+              <Button onClick={handleClickStart}>{t('Start trade')}</Button>
             </Box>
             <Box flex={1} bg="paginationItemBgHover" borderRadius="1.5x" display="flex">
               <Box
@@ -128,30 +304,7 @@ export const Ad: FC<Props> = () => {
                 flexDirection="column"
                 justifyContent="space-between"
               >
-                <Text variant="h6" color="adHeaderText">
-                  {t('Deal info')}
-                </Text>
-                <Box display="flex" justifyContent="space-between">
-                  <Text fontSize="medium" color="adHeaderLabel" fontWeight="strong">
-                    {t('Rate')}
-                  </Text>
-                  <P2PMoneyFormat money={rate} cryptoCurrency={cryptoCcy} />
-                </Box>
-                <Box display="flex" justifyContent="space-between">
-                  <Text fontSize="medium" color="adHeaderLabel" fontWeight="strong">
-                    {t('Limits')}
-                  </Text>
-                  <span>
-                    <P2PFiatFormat money={min} cryptoCurrency={cryptoCcy} /> —{' '}
-                    <P2PMoneyFormat money={max} cryptoCurrency={cryptoCcy} />
-                  </span>
-                </Box>
-                <Box display="flex" justifyContent="space-between">
-                  <Text fontSize="medium" color="adHeaderLabel" fontWeight="strong">
-                    {t('Bitzlato fee')}
-                  </Text>
-                  <span>0%</span>
-                </Box>
+                {dealInfoEl}
               </Box>
             </Box>
           </Box>
@@ -160,21 +313,7 @@ export const Ad: FC<Props> = () => {
           <Box p="6x" backgroundColor="adBg" borderRadius="1.5x" display="flex" gap="6x">
             <Box flex={1} display="flex">
               <Box display="flex" flexDirection="column" justifyContent="space-between">
-                <Box>
-                  <Box display="flex" mb="2x" alignItems="center" gap="2x">
-                    <Box as="p" fontSize="lead24">
-                      {advert.owner}
-                    </Box>
-                    {owner.verification && (
-                      <Tooltip label={t('Verified user')} placement="top">
-                        <div>
-                          <Box as={VerifiedIcon} display="block" />
-                        </div>
-                      </Tooltip>
-                    )}
-                  </Box>
-                  <OnlineStatusByLastActivity lastActivity={owner.lastActivity} />
-                </Box>
+                {traderEl}
                 <TrustedButton onClick={handleClickTrusted} trusted={owner.trusted} />
               </Box>
             </Box>
@@ -187,49 +326,11 @@ export const Ad: FC<Props> = () => {
                 flexDirection="column"
                 gap="4x"
               >
-                <Box display="flex" justifyContent="space-between">
-                  <Text fontSize="medium" fontWeight="strong">
-                    {t('Rating')}
-                  </Text>
-                  <Box display="flex" alignItems="center" gap="5x">
-                    <Box display="flex" alignItems="center" gap="1x">
-                      <LikeIcon />
-                      <span>{owner.feedbacks.find((v) => v.type === 'thumb_up')?.count ?? 0}</span>
-                    </Box>
-                    <Box display="flex" alignItems="center" gap="1x">
-                      <UnLikeIcon />
-                      <span>{owner.feedbacks.find((v) => v.type === 'hankey')?.count ?? 0}</span>
-                    </Box>
-                    <span>{owner.rating}</span>
-                  </Box>
-                </Box>
-                <Box display="flex" justifyContent="space-between">
-                  <Text fontSize="medium" fontWeight="strong">
-                    {t('Reputation')}
-                  </Text>
-                  {owner.safetyIndex}
-                </Box>
-                <Box display="flex" justifyContent="space-between">
-                  <Text fontSize="medium" fontWeight="strong">
-                    {t('Successful deals')}
-                  </Text>
-                  {deals.successDeals}
-                </Box>
-                <Box display="flex" justifyContent="space-between">
-                  <Text fontSize="medium" fontWeight="strong">
-                    {t('Canceled deals')}
-                  </Text>
-                  {deals.canceledDeals}
-                </Box>
+                {traderInfoEl}
               </Box>
             </Box>
           </Box>
-          <Box p="6x" backgroundColor="adBg" borderRadius="1.5x">
-            <Text variant="title">{t('Trade terms')}</Text>
-            <Box marginTop="6x" whiteSpace="pre-line">
-              <Text>{advert.terms}</Text>
-            </Box>
-          </Box>
+          {termsEl}
           <Box display="flex" alignItems="center" gap="6x">
             <ShieldIcon />
             <Text color="secondary">{t('ad.trade.info')}</Text>
