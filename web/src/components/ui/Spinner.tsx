66f1d909145cc155f3c3e2349ebaf6546a98d59c
Maksim Gerasimov <treizeger@gmail.com>
2022-06-07 15:05:19 +0300

    fix: Add size prop in ui/Spinner

--- web/src/components/ui/Spinner.tsx
+++ web/src/components/ui/Spinner.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react';
+import { Sprinkles } from 'web/src/theme/sprinkles.css';
 import { Box } from './Box';
 import * as s from './Spinner.css';
 
-export const Spinner: FC = () => <Box className={s.loader} />;
+type Props = {
+  size?: Sprinkles['size'] | undefined;
+};
+
+export const Spinner: FC<Props> = ({ size = '8x' }) => <Box className={s.loader} size={size} />;
