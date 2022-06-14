d1e47afa555f20794be7067966978ac8777422e7
Maksim Gerasimov <treizeger@gmail.com>
2022-06-14 17:46:08 +0300

    fix: Add empty content in TraderAds [#181865597]

---
+++ web/src/components/shared/TraderAds/TraderAdsEmpty.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import { Box } from 'web/src/components/ui/Box';
+import { Button } from 'web/src/components/ui/Button';
+import { Text } from 'web/src/components/ui/Text';
+import { useAppContext } from 'web/src/components/app/AppContext';
+import { useAdapterContext } from 'web/src/components/shared/Adapter';
+
+export const TraderAdsEmpty: FC = () => {
+  const { t, Link } = useAdapterContext();
+  const { isMobileDevice, lang } = useAppContext();
+
+  return (
+    <Box textAlign="center" py="20x" px="4x">
+      <Box mb="6x">
+        <Text variant={isMobileDevice ? 'title' : 'body'}>{t('traderAds.empty')}</Text>
+      </Box>
+      <Button as={Link} to={`/${lang}/p2p/`}>
+        {t('Go to')}
+      </Button>
+    </Box>
+  );
+};
