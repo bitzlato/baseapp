705ff45c4365b944dcd0cc816cc80b8bcdd4030e
Aleksandr Diskant <velsamm@yandex.ru>
2022-06-21 15:54:44 +0300

    chore: fix shared components export

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -284,9 +284,9 @@ module.exports = {
         './TradesScreen': './src/components/shared/Trades/Trades',
         './fetch': './src/helpers/fetch',
         './cssVars': './src/components/shared/sharedCssVars',
-        './Chat': './src/components/shared/Chat',
-        './ChatMessage': './src/components/shared/ChatMessage',
-        './Time': './src/components/shared/Time',
+        './Chat': './src/components/shared/Chat/Chat',
+        './ChatMessage': './src/components/shared/Chat/ChatMessage',
+        './Time': './src/components/shared/Time/Time',
       },
       shared: {
         react: { requiredVersion: deps.react, singleton: true },
