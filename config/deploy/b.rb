98ef79b5c485432a0b4b7e4ef18e7e8accf9d209
Maksim Gerasimov <treizeger@gmail.com>
2022-07-01 11:38:26 +0300

    chore: Add PUBLIC_URL to deployment process

--- config/deploy/b.rb
+++ config/deploy/b.rb
@@ -7,4 +7,3 @@ set :application, -> { 'baseapp-' + fetch(:stage).to_s }
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
 
 server ENV.fetch( 'STAGING_SERVER' ), user: fetch(:user), roles: fetch(:roles)
-
