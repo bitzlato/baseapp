ebc67aea4d83b6ffd1482889735e10a446de9d52
Konstantin Alekseev <sixandfo@gmail.com>
2022-07-11 15:35:51 +0300

    deploy for arnika

---
+++ config/deploy/arnika.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+set :stage, :production
+set :user, 'baseapp'
+set :public_url, 'https://arnika.lgk.one/'
+set :build_domain, 'arnika.lgk.one'
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:application)}_shared" }
+
+server ENV.fetch( 'PRODUCTION_SERVER' ),
+       user: fetch(:user),
+       roles: fetch(:roles),
+       ssh_options: { forward_agent: true }
