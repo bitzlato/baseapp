93cd087776627975f8569b1d1f44c71b66eead55
Yuri FA <yurik45gb@gmail.com>
2022-07-13 16:53:35 +0300

    fix: Change converting date string to timestamp from url query

--- web/src/helpers/getTimestamp.ts
+++ web/src/helpers/getTimestamp.ts
@@ -2,5 +2,9 @@ export const getTimestampWithoutTimezone = (date: Date) => {
   const time = date.getTime();
   const userTimezoneOffset = date.getTimezoneOffset() * 60000;
 
+  if (!time || !userTimezoneOffset) {
+    return undefined;
+  }
+
   return time - userTimezoneOffset;
 };
