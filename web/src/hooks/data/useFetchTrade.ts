f794e8feb5026a672955c526d74dd1742ddedc03
Maksim Gerasimov <treizeger@gmail.com>
2022-08-03 20:32:59 +0300

    chore: Remove unused code

--- web/src/hooks/data/useFetchTrade.ts
+++ web/src/hooks/data/useFetchTrade.ts
@@ -2,35 +2,12 @@ import { p2pUrl } from 'web/src/api';
 import { TradeInfo } from 'web/src/components/shared/Trade/types';
 import { fetchWithCreds } from 'web/src/helpers/fetch';
 import { useFetch } from './useFetch';
-import { ChatMessageList } from './useUserChat';
 
 export const useFetchTradeInfo = (tradeId: string | undefined) =>
   useFetch<TradeInfo>(tradeId ? `${p2pUrl()}/trade/${tradeId}` : undefined, fetchWithCreds, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
   });
-
-export function useFetchTradeChat(tradeId: string | undefined) {
-  return useFetch<ChatMessageList>(
-    tradeId ? `${p2pUrl()}/trade/${tradeId}/chat/` : null,
-    fetchWithCreds,
-    {
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-    },
-  );
-}
-
-export function useFetchTradeDisputeChat(tradeId: string | undefined) {
-  return useFetch(
-    tradeId ? `${p2pUrl()}/trade/${tradeId}/dispute/admin-chat/` : null,
-    fetchWithCreds,
-    {
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-    },
-  );
-}
 
 export function useFetchLastRequisites(paymethodId?: number | undefined) {
   return useFetch<{ data: string[] }>(
