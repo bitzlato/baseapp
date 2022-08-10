8b38818908fbdf40306b856bd8fb9bda185f8685
Aleksandr Grigorev <cognize6@gmail.com>
2022-08-10 13:34:52 +0300

    fix: "Start P2P Trading" link

--- web/src/screens/LandingScreen/WelcomeBlock/TradingStatisticsP2P.tsx
+++ web/src/screens/LandingScreen/WelcomeBlock/TradingStatisticsP2P.tsx
@@ -1,9 +1,12 @@
 import { FC, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useT } from 'src/hooks/useT';
 import { ControlledTradingChart } from 'web/src/containers/TradingChart/ControlledTradingChart';
 import { TradingViewMarket } from 'web/src/containers/TradingChart/api';
 import { Button } from 'web/src/components/ui/Button';
 import { MarketSelectorP2P } from 'web/src/containers/MarketSelector/MarketSelectorP2P';
+import { getLinkToP2P } from 'web/src/components/Header/getLinkToP2P';
+import { selectCurrentLanguage } from 'web/src/modules/public/i18n/selectors';
 
 import s from './WelcomeBlock.postcss';
 
@@ -30,14 +33,15 @@ const MARKETS: TradingViewMarket[] = [
 
 export const TradingStatisticsP2P: FC = () => {
   const t = useT();
+  const language = useSelector(selectCurrentLanguage);
 
   const [currentMarket, setCurrentMarket] = useState(MARKETS[0]!);
 
   const p2pMarketLink = useMemo(() => {
     const [crypto, paymethod] = currentMarket.id.toLowerCase().split('_');
     const fiat = currentMarket.quote_unit.toLowerCase();
-    return `/p2p/buy-${crypto}-${fiat}-${paymethod}`;
-  }, [currentMarket]);
+    return `${getLinkToP2P(language)}/buy-${crypto}-${fiat}-${paymethod}`;
+  }, [currentMarket, language]);
 
   return (
     <div className={s.tradingStatistics}>
