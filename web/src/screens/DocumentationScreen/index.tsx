508b2fc590e90702506e95a2c67ed164a0d27cfd
Maksim Gerasimov <treizeger@gmail.com>
2022-08-26 15:01:27 +0300

    feat: Add P2P Swagger in DocumentationScreen [#183060066]

--- web/src/screens/DocumentationScreen/index.tsx
+++ web/src/screens/DocumentationScreen/index.tsx
@@ -1,42 +1,12 @@
 import { FC, Suspense } from 'react';
-import { Tabs, TabList, Tab, TabPanel } from 'src/components/Tabs';
+import { Tabs, TabList, Tab, TabPanel } from 'web/src/components/Tabs';
 import { lazyRetry } from 'web/src/helpers/lazyRetry';
-
-import { DocumentationEndpoints, DocumentationHeader, DocumentationModels } from '../../components';
-import { useDocumentationFetch } from '../../hooks';
-
-export const DocumentationScreen: FC = () => (
-  <div className="pg-documentation">
-    <div className="pg-documentation__content">
-      <Tabs defaultValue="0">
-        <TabList>
-          <Tab size="large" value="0">
-            API
-          </Tab>
-          <Tab size="large" value="1">
-            Swagger UI
-          </Tab>
-          <Tab size="large" value="2">
-            WebSocket API
-          </Tab>
-        </TabList>
-        <TabPanel value="0">
-          <ApiDoc />
-        </TabPanel>
-        <TabPanel value="1">
-          <Suspense fallback>
-            <Swagger />
-          </Suspense>
-        </TabPanel>
-        <TabPanel value="2">
-          <Suspense fallback>
-            <WebSocketApi />
-          </Suspense>
-        </TabPanel>
-      </Tabs>
-    </div>
-  </div>
-);
+import {
+  DocumentationEndpoints,
+  DocumentationHeader,
+  DocumentationModels,
+} from 'web/src/components';
+import { useDocumentationFetch } from 'web/src/hooks';
 
 const ApiDoc: FC = () => {
   useDocumentationFetch();
@@ -53,3 +23,44 @@ const ApiDoc: FC = () => {
 const Swagger = lazyRetry(() => import('marketDocs/Swagger'));
 
 const WebSocketApi = lazyRetry(() => import('marketDocs/WebSocketApi'));
+
+export const DocumentationScreen: FC = () => (
+  <div className="pg-documentation">
+    <div className="pg-documentation__content">
+      <Tabs defaultValue="0">
+        <TabList>
+          <Tab size="large" value="0">
+            Peatio API
+          </Tab>
+          <Tab size="large" value="1">
+            Peatio Swagger UI
+          </Tab>
+          <Tab size="large" value="2">
+            WebSocket API
+          </Tab>
+          <Tab size="large" value="3">
+            P2P Swagger UI
+          </Tab>
+        </TabList>
+        <TabPanel value="0">
+          <ApiDoc />
+        </TabPanel>
+        <TabPanel value="1">
+          <Suspense fallback>
+            <Swagger url="api/v2/peatio/swagger" />
+          </Suspense>
+        </TabPanel>
+        <TabPanel value="2">
+          <Suspense fallback>
+            <WebSocketApi />
+          </Suspense>
+        </TabPanel>
+        <TabPanel value="3">
+          <Suspense fallback>
+            <Swagger url="v2/api-docs?group=all" />
+          </Suspense>
+        </TabPanel>
+      </Tabs>
+    </div>
+  </div>
+);
