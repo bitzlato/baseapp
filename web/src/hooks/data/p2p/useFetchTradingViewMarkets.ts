7df7ef0fea86e4d1afe639d809d309472c82ad56
Maksim Gerasimov <treizeger@gmail.com>
2022-09-15 11:24:17 +0300

    feat: Fix market list in trading view screen

--- web/src/hooks/data/p2p/useFetchTradingViewMarkets.ts
+++ web/src/hooks/data/p2p/useFetchTradingViewMarkets.ts
@@ -1,27 +1,6 @@
-// import { p2pUrl } from 'web/src/api/config';
-// import { useFetch } from 'web/src/hooks/data/useFetch';
-// import { P2PMarketSymbol } from 'web/src/types/tradingView.types';
+import { p2pUrl } from 'web/src/api/config';
+import { useFetch } from 'web/src/hooks/data/useFetch';
+import { P2PMarketSymbol } from 'web/src/types/tradingView.types';
 
-const MARKETS = [
-  {
-    symbol: 'BTC_Sberbank',
-    ccCode: 'BTC',
-    currency: 'RUB',
-    description: 'Sberbank',
-  },
-  {
-    symbol: 'BTC_QIWI',
-    ccCode: 'BTC',
-    currency: 'RUB',
-    description: 'QIWI',
-  },
-  {
-    symbol: 'BTC_Tinkoff',
-    ccCode: 'BTC',
-    currency: 'RUB',
-    description: 'Tinkoff',
-  },
-];
-
-export const useFetchTradingViewMarkets = () => ({ data: MARKETS, error: undefined });
-// useFetch<ReadonlyArray<P2PMarketSymbol>>(`${p2pUrl()}/public/trading-view/market-symbols`);
+export const useFetchTradingViewMarkets = () =>
+  useFetch<ReadonlyArray<P2PMarketSymbol>>(`${p2pUrl()}/public/trading-view/market-symbols`);
