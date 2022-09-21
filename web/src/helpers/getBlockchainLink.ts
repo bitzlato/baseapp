acbb0ebbf0ed4c21b08cb74a6b3f280842a2ab8c
Maksim Gerasimov <treizeger@gmail.com>
2022-09-21 18:20:12 +0300

    fix: Change template explorer blockchain

--- web/src/helpers/getBlockchainLink.ts
+++ web/src/helpers/getBlockchainLink.ts
@@ -2,10 +2,12 @@ import { Blockchain } from 'src/modules/public/blockchains/types';
 
 export function getBlockchainLink(blockchain: Blockchain, txid: string, rid?: string): string {
   if (txid && blockchain.explorer_transaction) {
-    return blockchain.explorer_transaction.replace('#{txid}', txid);
+    return blockchain.explorer_transaction.replace('$<txid>s', txid);
   }
+
   if (rid && blockchain.explorer_address) {
-    return blockchain.explorer_address.replace('#{address}', rid);
+    return blockchain.explorer_address.replace('$<address>s', rid);
   }
+
   return '';
 }
