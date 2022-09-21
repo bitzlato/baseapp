0d71caeefd8806cb50d95df95e446f5a06ca69dc
Maksim Gerasimov <treizeger@gmail.com>
2022-09-21 18:51:02 +0300

    fix: Fix replace in explorer url

--- web/src/helpers/getBlockchainLink.ts
+++ web/src/helpers/getBlockchainLink.ts
@@ -2,11 +2,11 @@ import { Blockchain } from 'src/modules/public/blockchains/types';
 
 export function getBlockchainLink(blockchain: Blockchain, txid: string, rid?: string): string {
   if (txid && blockchain.explorer_transaction) {
-    return blockchain.explorer_transaction.replace('$<txid>s', txid);
+    return blockchain.explorer_transaction.replace('%<txid>s', txid);
   }
 
   if (rid && blockchain.explorer_address) {
-    return blockchain.explorer_address.replace('$<address>s', rid);
+    return blockchain.explorer_address.replace('%<address>s', rid);
   }
 
   return '';
