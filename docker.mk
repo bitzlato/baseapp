956c4e1f16414705e9314042210acab9c335ceaa
Maksim Gerasimov <treizeger@gmail.com>
2022-10-13 19:02:48 +0300

    chore: Add makefile for docker

---
+++ docker.mk
@@ -0,0 +1,14 @@
+VERSION=latest
+REPO=barong
+REGISTRY=nexus.lgk.one:5000
+IMAGE_NAME=${REGISTRY}/${REPO}:${VERSION}
+
+docker-release: docker-build docker-push
+docker-build: check-args
+	docker build -t ${IMAGE_NAME} .
+docker-push: check-args
+	docker push ${IMAGE_NAME}
+check-args:
+ifndef VERSION
+	$(error VERSION is undefined)
+endif
