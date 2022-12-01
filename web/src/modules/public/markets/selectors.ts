c002820bd8aaf269da3283f058c06d654c77ab37
Maksim Gerasimov <treizeger@gmail.com>
2022-12-01 17:44:31 +0300

    fix: Change of getting last price in current market [#183806164]

--- web/src/modules/public/markets/selectors.ts
+++ web/src/modules/public/markets/selectors.ts
@@ -1,4 +1,7 @@
-import { RootState } from '../..';
+import { Money } from '@bitzlato/money-js';
+import { createMoneyWithoutCcy } from 'web/src/helpers/money';
+import { RootState } from 'web/src/modules';
+import { selectLastRecentTrade } from 'web/src/modules/public/recentTrades/selectors';
 import { MarketsState } from './reducer';
 import { Market } from './types';
 
@@ -32,3 +35,46 @@ export const selectMarketPrice = (state: RootState): MarketsState['marketPrice']
 
 export const selectMarketPriceFetch = (state: RootState): boolean =>
   selectMarketsState(state).successMarketPriceFetch;
+
+export type LastPrice = { price: Money; changeSign?: 'positive' | 'negative' | undefined };
+
+export const selectLastPrice = (state: RootState): LastPrice | undefined => {
+  const currentMarket = selectCurrentMarket(state);
+  const marketTickers = selectMarketTickers(state);
+  const lastRecentTrade = selectLastRecentTrade(state);
+
+  if (!currentMarket) {
+    return undefined;
+  }
+
+  let lastPrice = '';
+  let changeSign: 'positive' | 'negative' | undefined;
+
+  if (lastRecentTrade?.market === currentMarket.id) {
+    lastPrice = lastRecentTrade.price;
+
+    if (Number(lastRecentTrade.price_change) >= 0) {
+      changeSign = 'positive';
+    } else if (Number(lastRecentTrade.price_change) < 0) {
+      changeSign = 'negative';
+    }
+  } else {
+    const currentTicker = marketTickers[currentMarket.id];
+    if (!currentTicker) {
+      return undefined;
+    }
+
+    lastPrice = currentTicker.last;
+
+    if (currentTicker.price_change_percent.includes('+')) {
+      changeSign = 'positive';
+    } else if (currentTicker.price_change_percent.includes('-')) {
+      changeSign = 'negative';
+    }
+  }
+
+  return {
+    price: createMoneyWithoutCcy(lastPrice, currentMarket.price_precision),
+    changeSign,
+  };
+};
