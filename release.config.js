67d68896d87223ee7fa105bb99c4b745bd1ed991
Maksim Gerasimov <treizeger@gmail.com>
2022-12-06 13:56:47 +0300

    chore: Add semantic-release

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: false,
+  branches: ['master'],
+  plugins: [
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        writeOpts: {
+          issueUrlFormat: 'https://www.pivotaltracker.com/story/show/{{id}}',
+        },
+      },
+    ],
+    ['@semantic-release/npm', { npmPublish: false }],
+    ['@semantic-release/git', { assets: ['package.json'] }],
+    ['@semantic-release/github', { labels: false, failComment: false, successComment: false }],
+  ],
+};
