e6ab06906ab3ebe7be6f78f9b78105b7c6097bc7
Maksim Gerasimov <treizeger@gmail.com>
2022-12-16 14:36:26 +0300

    fix: Disable balances request when user is not activated [#183286666]

--- web/src/hooks/useWalletsFetch.ts
+++ web/src/hooks/useWalletsFetch.ts
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useIsUserActivated } from 'web/src/components/app/UserContext';
 import { selectShouldFetchWallets, walletsFetch } from '../modules';
 
 export const useWalletsFetch = () => {
+  const isUserActivated = useIsUserActivated();
   const shouldDispatch = useSelector(selectShouldFetchWallets);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    if (shouldDispatch) {
+    if (shouldDispatch && isUserActivated) {
       dispatch(walletsFetch());
     }
-  }, [dispatch, shouldDispatch]);
+  }, [dispatch, isUserActivated, shouldDispatch]);
 };
