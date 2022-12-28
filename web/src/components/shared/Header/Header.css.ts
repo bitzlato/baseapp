dd6fad9830c2dfad1f65297fad312effc7831232
Yuri FA <yurik45gb@gmail.com>
2022-12-28 14:29:32 +0300

    feat: add snowfall to header logo

--- web/src/components/shared/Header/Header.css.ts
+++ web/src/components/shared/Header/Header.css.ts
@@ -1,6 +1,6 @@
 import { globalStyle, style } from '@vanilla-extract/css';
 import { sprinkles } from 'web/src/theme/sprinkles.css';
-import { responsiveStyle } from 'web/src/theme/themeUtils';
+import { queries, responsiveStyle } from 'web/src/theme/themeUtils';
 import { vars } from 'web/src/theme/vars.css';
 
 export const backButton = style(
@@ -22,6 +22,46 @@ export const header = style([
     },
     tablet: {
       backgroundColor: vars.colors.headerBg,
+    },
+  }),
+]);
+
+export const snowfall = style([
+  {
+    selectors: {
+      '&::after': {
+        content: '',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: 'block',
+        boxShadow: `inset -10px -3px 20px 4px ${vars.colors.headerMobileBg}`,
+      },
+    },
+    '@media': {
+      [queries.tablet]: {
+        selectors: {
+          '&::after': {
+            boxShadow: `inset -10px -3px 20px 4px ${vars.colors.headerBg}`,
+          },
+        },
+      },
+    },
+  },
+  responsiveStyle({
+    mobile: {
+      width: '80px',
+      height: '47px',
+    },
+    tablet: {
+      width: '100px',
+      height: '63px',
+    },
+    desktop: {
+      width: '130px',
+      height: '63px',
     },
   }),
 ]);
