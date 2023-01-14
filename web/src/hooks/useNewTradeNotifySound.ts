bbc3b1b33a20a77515dc4e66766296d949539851
Yuri FA <yurik45gb@gmail.com>
2023-01-14 16:12:00 +0300

    fix: Add listener when trying play sound and user has not interact with site

--- web/src/hooks/useNewTradeNotifySound.ts
+++ web/src/hooks/useNewTradeNotifySound.ts
@@ -6,12 +6,46 @@ import { TradeInfo, TradeStatus } from 'web/src/components/shared/Trade/types';
 import { fetchWithCreds } from 'web/src/helpers/fetch';
 
 const NEW_TRADE_SOUND_URL = `${process.env.ASSET_PATH}new_trade_sound.wav`;
+const USER_INTERACTION_EVENTS = ['click', 'mousedown', 'keydown'];
 
 const playNewTradeSound = async () => {
+  const newTradeSound = new Audio(NEW_TRADE_SOUND_URL);
+
+  const playAgain = async () => {
+    try {
+      await newTradeSound.play();
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      removeListeners();
+    } catch (error) {
+      if (error instanceof Error && error.name !== 'NotAllowedError') {
+        Bugsnag.notify(error);
+      }
+    }
+  };
+
+  const addListeners = () => {
+    USER_INTERACTION_EVENTS.forEach((event) => {
+      document.addEventListener(event, playAgain, { once: true });
+    });
+  };
+
+  const removeListeners = () => {
+    USER_INTERACTION_EVENTS.forEach((event) => {
+      document.removeEventListener(event, playAgain);
+    });
+  };
+
   try {
-    await new Audio(NEW_TRADE_SOUND_URL).play();
-  } catch (err: any) {
-    Bugsnag.notify(err);
+    await newTradeSound.play();
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === 'NotAllowedError') {
+        // Await user interaction in site because Audio.play() blocked by browser when user has not interact before playing
+        addListeners();
+      } else {
+        Bugsnag.notify(error);
+      }
+    }
   }
 };
 
